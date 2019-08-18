33b3e0321495649fcd6950786e3731f6da9b2d1e
lchuang <z5202dbc@gmail.com>
2019-08-18 09:11:50 -0700

    Successfully adding 時間,時刻 columns, but still need to manually modify format in Excel(Numbers)

--- processingPLUS.py
+++ processingPLUS.py
@@ -1,14 +1,30 @@
-import datetime, csv
+
 import pandas as pd
 
 def read():
-    df = pd.read_excel('siteDictAll.xlsx')
+    df = pd.read_excel('siteDictAll-part1.xlsx', skiprows=1, dtype={'時間':str})
 
-    # print("Column headings:")
-    # print(df.columns)
+    date = []
+    hour = []
 
-    print(df)
-    for i in df.index:
-        print(df['站名'][i])
+    for row in df.itertuples():
 
-read()
+        d = row.時間[:10]
+        t = row.時間[11:]
+        date.append(d)
+        hour.append(t)
+
+    df['日期'] = date
+    df['時刻'] = hour
+
+    return df
+
+
+def output(df):
+    df.to_excel('testing.xlsx', sheet_name='ubike_Station_stats', index=False)
+
+
+
+cols = [3]
+dframe = read()
+output(dframe)
