fdb9d66256be03a198209464dbacabdef9f585eb
lchuang <z5202dbc@gmail.com>
2019-08-22 10:38:57 -0700

    再把 時間,日期,時刻 三個欄位根據Janice傳的空汙檔案去做調整 時間(datetime) 2018-03-01 00:00:00 日期(str) 3/1/2018 時間(str) 00:00

--- modify_date.py
+++ modify_date.py
@@ -1,20 +1,40 @@
 import pandas as pd
+pd.set_option('display.max_columns', 999)
+
 
 def read():
-    df = pd.read_excel('siteDictAll_April.xlsx', dtype={'日期':str})
+    df = pd.read_excel('final_files/siteDictAll_April_new 08192019.xlsx')
     return df
 
 def modify(df):
     for row in df.itertuples():
-        # print(row.時間, row.日期)
-        t1 = row.日期
-        # print(len(t1))
-        if len(t1) == 8:
-            t1_new = t1[1:]
-        else:
-            t1_new = t1[3] + '/' + t1[5:7] + '/' + t1[8:10]
-        # print(row.時間, t1_new)
-        df.at[row.Index, '日期'] = t1_new
+        # print(row)
+        # print(row.時間, type(row.時間))
+
+        datetime_time = row.時間.to_pydatetime()
+        # print(datetime_time, type(datetime_time))
+        df.at[row.Index, '時間'] = datetime_time
+
+        str_time = datetime_time.strftime("%Y/%m/%d")
+        # print(str_time, type(str_time))
+
+        # print(df.at[row.Index, '日期'])
+
+        df.at[row.Index, '日期'] = str_time
+        # print(df.at[row.Index, '日期'])
+
+
+        # print(row)
+        # datetime_t1 = datetime.datetime.strptime(t1, "%m/%d/%Y")
+
+        # print(datetime_t1, type(datetime_t1))
+        # print()
+        # if len(t1) == 8:
+        #     t1_new = t1[1:]
+        # else:
+        #     t1_new = t1[3] + '/' + t1[5:7] + '/' + t1[8:10]
+        # df.at[row.Index, '日期'] = t1_new
+
         # print(type(t1))
         # t2 = row['日期']
         # print(t2)
@@ -30,9 +50,12 @@ def modify(df):
 
 def export(results):
 
-    results.to_excel('siteDictAll_April_new.xlsx', sheet_name='ubike_Station_stats', index=False)
+    results.to_excel('final_files/siteDictAll_April_new 08192019v3.xlsx', sheet_name='ubike_Station_stats', index=False)
     print(results)
 
 df = read()
+
 results = modify(df)
+# print(results)
+
 export(results)
